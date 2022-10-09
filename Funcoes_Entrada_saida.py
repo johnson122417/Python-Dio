9dc4da798b3c1dc5d7bc1bd24357d1a6a7c96d69
Lindon Johnson <93793654+johnson122417@users.noreply.github.com>
2022-10-09 16:14:29 -0300

    Add files via upload
    Estudos De Python DIO.ME...
    BH_Unimed-Ciencias de dados

---
+++ Funcoes_Entrada_saida.py
@@ -0,0 +1,9 @@
+Nome = input("Digite o Seu Nome: ")
+Idade = input("Digite a Sua Idade: ")
+
+print(Nome)
+print(Idade)
+print(Nome, Idade, end="... \n")
+print(Nome, Idade, sep="#", end="... \n")
+print(Nome, Idade, sep="#")
+
---
+++ convertendo_tipos.py
@@ -0,0 +1,15 @@
+print(int(1.6584789652))
+print(int('10'))
+print(float("10.10"))
+print(float(100))
+
+
+valor = 10
+valor_str = str(valor)
+print(type(valor))
+print(type(valor_str))
+
+
+print(100 / 2)
+print(100 // 2)
+print(100 % 2)
